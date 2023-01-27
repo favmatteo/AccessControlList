8697f092578b04db804c312b81c1aa0a787e9983
favmatteo <matteo.favaro@outlook.com>
2023-01-27 22:04:16 +0100

    Added dashboard page skeleton & creted session cookies

---
+++ src/scripts/php/exceptions/confirmEmailException.php
@@ -0,0 +1,12 @@
+<?php
+
+class confirmEmailException extends Exception {
+    public function __construct(Throwable $previous = null) {
+        parent::__construct("Confirm email before", 403, $previous);
+    }
+
+    // custom string representation of object
+    public function __toString() {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+}
--- src/scripts/php/login.php
+++ src/scripts/php/login.php
@@ -1,6 +1,12 @@
 <?php
 session_start();
+
+use Kreait\Firebase\Exception\Auth\FailedToVerifySessionCookie;
+use Kreait\Firebase\Auth\SignIn\FailedToSignIn;
+
 include ('dbcon.php');
+include ('exceptions/confirmEmailException.php');
+
 
 if(isset($_POST['submit-btn'])){
     $email = $_POST['email'];
@@ -10,12 +16,25 @@ if(isset($_POST['submit-btn'])){
         $signInResult = $auth->signInWithEmailAndPassword($email, $password);
         $user = $auth->getUserByEmail($email);
         $emailVerified = $user->emailVerified;
-        if($emailVerified){
-            echo "Welcome";
-        }else{
-            echo "Check your email";
+        
+        // Check if email is confirmed
+        if(!$emailVerified){
+            throw new confirmEmailException();
         }
-    }catch(Kreait\Firebase\Auth\SignIn\FailedToSignIn $e) {
+
+        // if it is confirmed
+        // start cookie session (valid for 7 days)
+        $oneWeek = new DateInterval('P7D');
+        $token = $signInResult->asTokenResponse()['id_token'];
+        //print_r($token);
+        $sessionCookieString = $auth->createSessionCookie($token, $oneWeek);
+        $verifiedSessionCookie = $auth->verifySessionCookie($sessionCookieString);
+
+    }catch(FailedToSignIn $e) {
         echo "Wrong email or password";
+    }catch(confirmEmailException  $e) {
+        echo $e;
+    }catch(FailedToVerifySessionCookie $e){
+        echo 'The Session Cookie is invalid: '.$e->getMessage(); 
     }
 }
