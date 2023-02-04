40245b4c82bb6759913c8e38656512432dd0a769
favmatteo <matteo.favaro@outlook.com>
2023-02-04 22:36:24 +0100

    Fixed DB

--- src/scripts/php/db.sql
+++ src/scripts/php/db.sql
@@ -3,7 +3,7 @@ USE acl;
 CREATE TABLE
     IF NOT EXISTS roles (
         id_role INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(50) NOT NULL
+        name VARCHAR(50) NOT NULL UNIQUE
     );
 
 CREATE TABLE
@@ -58,4 +58,82 @@ CREATE TABLE
         FOREIGN KEY(id_user) REFERENCES user(id_user),
         id_customer INT UNSIGNED NOT NULL,
         FOREIGN KEY(id_customer) REFERENCES customer(id_customer)
+    );
+
+INSERT INTO roles (name) VALUES ('Undefined');
+
+INSERT INTO roles (name) VALUES ('admin');
+
+INSERT INTO roles (name) VALUES ('commercial');
+
+INSERT INTO roles (name) VALUES ('Area manager');
+
+INSERT INTO roles (name) VALUES ('Administration');
+
+INSERT INTO
+    permission (
+        id_permission,
+        pcreate,
+        pread,
+        pdelete,
+        pupdate
+    )
+VALUES (1, 1, 1, 1, 1);
+
+INSERT INTO
+    permission (
+        id_permission,
+        pcreate,
+        pread,
+        pdelete,
+        pupdate
+    )
+VALUES (2, 1, 1, 0, 1);
+
+INSERT INTO
+    permission (
+        id_permission,
+        pcreate,
+        pread,
+        pdelete,
+        pupdate
+    )
+VALUES (3, 0, 1, 1, 1);
+
+INSERT INTO
+    permission (
+        id_permission,
+        pcreate,
+        pread,
+        pdelete,
+        pupdate
+    )
+VALUES (4, 0, 1, 1, 0);
+
+INSERT INTO dispose (id_role, id_permission) VALUES (1, 1);
+
+INSERT INTO dispose (id_role, id_permission) VALUES (2, 2);
+
+INSERT INTO dispose (id_role, id_permission) VALUES (3, 3);
+
+INSERT INTO dispose (id_role, id_permission) VALUES (4, 4);
+
+INSERT INTO
+    customer (
+        email,
+        first_name,
+        surname,
+        gender,
+        telephone,
+        address,
+        age
+    )
+VALUES (
+        "prova@prova.it",
+        "prova",
+        "prova",
+        'M',
+        "1234567890",
+        "via prova 1",
+        20
     );
