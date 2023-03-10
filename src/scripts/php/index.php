878e0d65995578d4411f0e8ee3ed7f1acd24cf9a
favmatteo <matteo.favaro@outlook.com>
2023-03-10 23:39:44 +0100

    Added some translations

--- src/scripts/php/index.php
+++ src/scripts/php/index.php
@@ -1,8 +1,6 @@
 <?php
 require('dbcon.php');
-require('to-italian.php');
-
-session_start();
+require('lang.php');
 
 
 
---
+++ src/scripts/php/lang.php
@@ -0,0 +1,26 @@
+<?php
+
+session_start();
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+if(!isset($_SESSION['language'])){
+    $language = 'en_US.utf8';
+    $_SESSION['language'] = $language;
+}else{
+    $language = $_SESSION['language'];
+}
+
+$_SESSION['language'] = $language;
+//echo $_SESSION['language'];
+
+//$language = "it_IT.utf8";
+putenv("LANG=" . $language); 
+setlocale(LC_ALL, $language);
+
+$domain = "messages";
+bindtextdomain($domain, "locale"); 
+bind_textdomain_codeset($domain, 'UTF-8');
+textdomain($domain);
